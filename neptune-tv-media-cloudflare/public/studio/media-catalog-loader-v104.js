c0fb88160fcf80cedd0b5535d0f7d0d16c6d1d09
jojoleblond <johanzambelli@gmail.com>
2026-08-13 00:08:57 +0200

    Fix Catalogue Media loader escaping

--- neptune-tv-media-cloudflare/public/studio/media-catalog-loader-v104.js
+++ neptune-tv-media-cloudflare/public/studio/media-catalog-loader-v104.js
@@ -206,7 +206,7 @@ function showBootstrapError(error){
 }
 
 function escapeHtml(value){
-  return String(value??'').replace(/[&<>"']/gu,character=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#39;'}[character]));
+  return String(value??'').replace(/[&<>"']/gu,character=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[character]));
 }
 
 async function importUxWithoutObserverFeedbackLoop(){

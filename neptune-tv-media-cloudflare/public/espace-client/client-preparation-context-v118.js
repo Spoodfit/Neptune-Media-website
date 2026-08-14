6a277c26c14e114408b1e801e43979524696a6a3
jojoleblond <johanzambelli@gmail.com>
2026-08-14 22:22:29 +0200

    Client prep: finalize safe HTML escaping

--- neptune-tv-media-cloudflare/public/espace-client/client-preparation-context-v118.js
+++ neptune-tv-media-cloudflare/public/espace-client/client-preparation-context-v118.js
@@ -189,5 +189,5 @@ function formatDateTime(date){
 }
 
 function escapeHtml(value){
-  return String(value??'').replace(/[&<>"']/gu,character=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#39;'})[character]);
+  return String(value??'').replace(/[&<>"']/gu,character=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'})[character]);
 }
